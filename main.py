7ce0f8228256788346e177e47d1bb11e7f8a13d2
9angelcosta <9angelcosta@gmail.com>
2025-10-24 21:55:29 -0700

    undefined pivot variable error

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 def getPivot(number):
     avg = (number[0] + number[1] + number[2] + number[3] + number[4] + number[5]) // 5
     
-    closenum = number[0]
+    pivot = number[0]
     mindiff = number[0] - avg
     
     for num in number[1:]:
         difference = num - avg
         if difference < mindiff:
             mindiff = difference
-            closenum = num   
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
+            pivot = num   
+            
+    return pivot
+    
+left_sublist = []
+right_sublist = []
+    
 def split(number):
+    for x in number[1:]:
+        if x <= pivot:
+            left_sublist.append(x)
+        else:
+            right_sublist.append(x)
+            
+    new_list = left_sublist + pivot + right_sublist
+    return new_list
+            
+    
     """
     ########################################
     Code Your Program here
